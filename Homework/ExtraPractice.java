92cb6b311637748768cc7cf55eccb09a017da4ae
RKS244256 <118144193+RKS244256@users.noreply.github.com>
2025-02-07 14:32:41 -0500

    AHHHHHHHHHH

---
+++ Homework/ExtraPractice.java
@@ -0,0 +1,47 @@
+package Homework;
+
+class ExtraPractice {
+    // Write a method that takes in a radius and height and 
+    //returns and volume of the cylinder
+    // with that radius and height
+    static int cylinderVolume (int radius, int height){
+        return (int) (3.14*Math.pow(radius, 2)*height);
+    }
+    // Write a method that takes in a radius and height and 
+    //returns the surface area of the cylinder
+    // with that radius and height
+    static int cylinderSurfaceArea (int radius, int height){
+        return (int) (2*3.14*radius*height)+(int) (2*3.14*Math.pow(radius, 2));
+    }
+
+    // Write a method that takes in a number of seconds 
+    // and prints the numbers of minutes and seconds in that time
+    // for example if the input is 187 
+    //-> the output will be 3 min and 7 seconds
+    static void secondsToMinutes (int secs){
+        int mins = Math.round(secs/60);
+        int osecs = secs - (mins*60);
+        System.out.printf("%d min and %d seconds\n", mins, osecs);
+    }
+
+    // Write a method that takes in a number of seconds 
+    //and prints the number of hours, minutes and seconds in that time
+    // for example if the input is 4000 -> the output will be 1 hr 6 min and 40 seconds
+    static void secondsToHours (int secs){
+        int hours = Math.round(secs/3600);
+        secs = secs - (hours*3600);
+        int mins = Math.round(secs/60);
+        secs = secs - (mins*60);
+        System.out.printf("%d hours, %d min, and %d seconds\n", hours, mins, secs);
+    }
+    // Write a method that takes in 3 numbers and prints them from smallest to largest
+    // for example if the input is 60, 80, 30 -> the output will be 30, 60, 80
+
+
+    public static void main(String[] args) {
+        System.out.println(cylinderVolume(5, 10));
+        System.out.println(cylinderSurfaceArea(5, 10));
+        secondsToMinutes(120);
+        secondsToHours(4000);
+    }
+}
--- Homework/Introduction.java
+++ Homework/Introduction.java
@@ -15,10 +15,32 @@ public class Introduction {
         double s = (a+b+c)/2;
         return (float) Math.sqrt(s*(s-a)*(s-b)*(s-c));
     }
+    public static void secondsToMinutes (int secs){
+        int mins = Math.round(secs/60);
+        int osecs = secs - (mins*60);
+        System.out.printf("%d and %d/60\n", mins, osecs);
+    }
+    public static int lastNDigit (int num, int n){
+        // char[] snum = String.valueOf(num).toCharArray();
+        // int nnum = snum.length - n;
+        // return Integer.parseInt((String.valueOf(snum[nnum])));
+        return num % (int) Math.pow(10, n);
+
+    }
+    public static int randInt (int x, int y){
+        return (int)(Math.random() * (Math.abs(y - x) + 1)) + Math.min(x, y);
+    }
+    public static int max (int x, int y, int z){
+        return (Math.max(Math.max(x, y), z));
+    }
     public static void main(String[] args) {
         Introduction.greeting("Ryan");
         System.out.println(yearsToDays(5));
         System.out.println(areaOfTriangle(1, 1));
         System.out.println(areaOfTriangle((float) Math.sqrt(2), 1, 1));
+        secondsToMinutes(50);
+        System.out.println(lastNDigit(123456, 2));
+        System.out.println(randInt(123, 200));
+        System.out.println(max(123, 200, 25));
     }
 }
